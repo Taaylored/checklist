23bca44660b43763e74b80bfbeaaae95c0765b39
Taaylored <tprince09@eagles.ewu.edu>
2017-05-23 15:03:59 -0700

    I don't know

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,59 @@
+
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
 // @codekit-prepend "airtable.js";
 
-
-
-// CONFIGURE AND 'HANDSHAKE' AIRTABLE
-
+// CONFIGURE & "Handshake"
 var Airtable = require('airtable');
 Airtable.configure({
     endpointUrl: 'https://api.airtable.com',
     apiKey: 'keyNyK2dVB4Da7Dzo'
 });
 var base = Airtable.base('appYjgGyxE7AXQYex');
-
-
-//is base coming through? check check
+// Check-Check
 console.log(base);
 
-
-
-//Get Records
-
+// Get Records
 base('YaHURR').select({
     view: 'Grid view'
 }).firstPage(function(err, records) {
+    
     if (err) { console.error(err); return; }
+
     records.forEach(function(record) {
-        
-//        console.log('Retrieved', record.get('Name'));
-            //Get Names
-            var students = `<h1>${record.get('Name')}</h1>`
-            //Display Names
-            $('#students').html(students);
+       
+      // Check-Check 
+      // console.log('Retrieved', record.get('Name'));
+      // console.log( record.fields.Avatar[0].url ); 
+
+      // Display Data
+      showStudents(record)
+
     });
 });
 
+// Template Literal
+var showStudents = function(record) {
 
+  var template = 
+  `
+    <section class="ui raised card">
+        <div class="image">
+           <img src=" ${record.fields.Avatar[0].url} " alt="">
+        </div>
+       <div class="content">
+           <h3>${record.fields.Name}</h3>
+           <p class="description">
+               ${record.fields.Bio}
+           </p>
+           <p class="description">
+              <a href=" ${record.fields.GitHub} ">GitHub</a> and <a href="mailto:${record.fields.Email}">Email</a>
+           </p>
+       </div>
+    </section>
+  `;
 
+  // Display Collected Data
+  $('#students').append(template);
 
-//Template Literal
-var students = `<section class="ui card">
-               <div class="image">
-                   <img src="http://placehold.it/400x300" alt="">
-                </div>
-
-                <div class="content">
-                    <h3>First Name</h3>
-                    <p class="description">
-                        <a href="">GitHub</a> and <a href="mailto:">Email</a>
-                    </p>
-                </div> 
-           </section>
-	    </article>
-`;
-
+}
